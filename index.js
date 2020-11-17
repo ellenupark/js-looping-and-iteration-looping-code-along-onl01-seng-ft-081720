e3770d0730538c5b4a9b978793a626fd6312fe17
ellenupark <ellenupark@gmail.com>
2020-11-17 12:55:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(string, event) {
+    const result = []
+    for (let i = 0; i < string.length; i++ ) {
+        result.push(`Thank you, ${string[i]}, for the wonderful ${event} gift!`);
+    }
+    return result
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
